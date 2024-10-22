3f740dae51c24f3e458f7447cf2c381ce39f9250
darina-rtm <darina-rtm@yandex.ru>
2024-10-22 12:01:18 +0300

    внесенное изменение исправлено

--- first-repository.py
+++ first-repository.py
@@ -3,6 +3,6 @@ def is_palindrome(text):
   return text == text[::-1]
 text = input("Введите любой текст: ")
 if is_palindrome(text):
-  print("Это палинддддддддддддром!")
+  print("Это палиндром!")
 else:
   print("Это не палиндром.")
